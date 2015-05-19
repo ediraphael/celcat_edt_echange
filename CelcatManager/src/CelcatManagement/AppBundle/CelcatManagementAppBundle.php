6f279027634634be2294849f2f01623005905e23
raphael <raphael.pillie@gmail.com>
2015-05-19 09:50:06 +0200

    Modification projet, pour des test

---
+++ CelcatManager/src/CelcatManagement/AppBundle/CelcatManagementAppBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CelcatManagement\AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CelcatManagementAppBundle extends Bundle
+{
+}
---
+++ CelcatManager/src/CelcatManagement/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace CelcatManagement\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('CelcatManagementAppBundle:Default:index.html.twig', array('name' => $name));
+    }
+    
+    public function index2Action()
+    {
+        return $this->render('CelcatManagementAppBundle:Default:index.html.twig', array('name' => "toto"));
+    }
+}
