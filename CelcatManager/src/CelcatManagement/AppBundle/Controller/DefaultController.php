9ba94c4fcfc9664ba5a8d1b33e76cab4e78a4037
raphael <raphael.pillie@gmail.com>
2015-05-26 17:42:23 +0200

    Déclaration d'une action pour le login

--- CelcatManager/src/CelcatManagement/AppBundle/Controller/DefaultController.php
+++ CelcatManager/src/CelcatManagement/AppBundle/Controller/DefaultController.php
@@ -4,15 +4,30 @@ namespace CelcatManagement\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
-{
-    public function indexAction($name)
-    {
+class DefaultController extends Controller {
+
+    public function indexAction($name) {
         return $this->render('CelcatManagementAppBundle:Default:index.html.twig', array('name' => $name));
     }
-    
-    public function index2Action()
-    {
+
+    public function index2Action() {
         return $this->render('CelcatManagementAppBundle:Default:index.html.twig', array('name' => "toto"));
+    }
+
+    public function loginAction(\Symfony\Component\HttpFoundation\Request $request, \Symfony\Component\Security\Core\Exception\AuthenticationException $exception = null) {
+
+        if ($this->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->redirect($this->generateUrl('celcat_management_app_index'));
+        }
+        if ($exception === null) {
+            return $this->redirect($this->generateUrl('celcat_management_app_index'));
+        }
+        $manager = $this->get('be_simple.sso_auth.factory')->getManager("admin_sso", "/");
+
+        return array(
+            'manager' => $manager,
+            'request' => $request,
+            'exception' => $exception
+        );
     }
 }
