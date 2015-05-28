fb52bdad3b609743e13042b20f78015a97522258
raphael <raphael.pillie@gmail.com>
2015-05-28 16:14:16 +0200

    MOdification du listener Calendar

--- CelcatManager/src/CelcatManagement/AppBundle/EventListener/CalendarEventListener.php
+++ CelcatManager/src/CelcatManagement/AppBundle/EventListener/CalendarEventListener.php
@@ -2,7 +2,7 @@
 
 namespace CelcatManagement\AppBundle\EventListener;
 
-use ADesigns\CalendarBundle\Event\CalendarEvent;
+use CelcatManagement\AppBundle\Event\CalendarEvent;
 use ADesigns\CalendarBundle\Entity\EventEntity;
 use Doctrine\ORM\EntityManager;
 
@@ -19,8 +19,26 @@ class CalendarEventListener
     {
         $startDate = $calendarEvent->getStartDatetime();
         $endDate = $calendarEvent->getEndDatetime();
-
+        $url = $calendarEvent->getRequest()->request->get('urlPath');
+        $schedulerManager = new \CelcatManagement\CelcatReaderBundle\Models\ScheduleManager();
         
+        
+        $schedulerManager->parseAllSchedule($url);
+        
+        $arrayWeeks = $schedulerManager->getArrayWeeks();
+        
+        
+        foreach ($arrayWeeks as $indexWeek => $week) {
+            foreach ($week->getArrayDays() as $indexDay => $day) {
+                foreach ($day->getArrayEvents() as $indexEvents => $events) {
+                    foreach ($events as $groupeName => $event) {
+                        $calendarEvent->addEvent($event);
+                    }
+                }
+            }
+        }
+       
+        //$calendarEvent->getRequest()->request->get('calendar')
 
         // $companyEvents and $companyEvent in this example
         // represent entities from your database, NOT instances of EventEntity
@@ -37,16 +55,16 @@ class CalendarEventListener
 //            } else {
 //                $eventEntity = new EventEntity($companyEvent->getTitle(), $companyEvent->getStartDatetime(), null, true);
 //            }
-            $eventEntity = new EventEntity("toto", new \DateTime(), null, true);
-            //optional calendar event settings
-            $eventEntity->setAllDay(true); // default is false, set to true if this is an all day event
-            $eventEntity->setBgColor('#FF0000'); //set the background color of the event's label
-            $eventEntity->setFgColor('#FFFFFF'); //set the foreground color of the event's label
-            $eventEntity->setUrl('http://www.google.com'); // url to send user to when event label is clicked
-            $eventEntity->setCssClass('my-custom-class'); // a custom class you may want to apply to event labels
+//            $eventEntity = new EventEntity("toto", new \DateTime(), null, true);
+//            //optional calendar event settings
+//            $eventEntity->setAllDay(true); // default is false, set to true if this is an all day event
+//            $eventEntity->setBgColor('#FF0000'); //set the background color of the event's label
+//            $eventEntity->setFgColor('#FFFFFF'); //set the foreground color of the event's label
+//            $eventEntity->setUrl('http://www.google.com'); // url to send user to when event label is clicked
+//            $eventEntity->setCssClass('my-custom-class'); // a custom class you may want to apply to event labels
 
             //finally, add the event to the CalendarEvent for displaying on the calendar
-            $calendarEvent->addEvent($eventEntity);
+            //$calendarEvent->addEvent($eventEntity);
 //        }
     }
 }
