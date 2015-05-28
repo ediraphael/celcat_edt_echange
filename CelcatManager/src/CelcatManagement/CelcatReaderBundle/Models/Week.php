698c9f7f86ffb7f00a45e521d9b5ccafa31bce9f
raphael <raphael.pillie@gmail.com>
2015-05-28 10:10:14 +0200

    Conformisation de la class week

--- CelcatManager/src/CelcatManagement/CelcatReaderBundle/Models/Week.php
+++ CelcatManager/src/CelcatManagement/CelcatReaderBundle/Models/Week.php
@@ -2,48 +2,46 @@
 
 namespace CelcatManagement\CelcatReaderBundle\Models;
 
-class Week
-{
-    
-    private $tab_days,
-            $description,
-            $id,
-            $tag,
-            $date;
-    
-    function __construct()
-    {
+class Week {
+
+    private $tab_days;
+    private $description;
+    private $id;
+    private $tag;
+    private $date;
+
+    function __construct() {
         $this->tab_days = array();
-        $day = new Day();
-        $day->setId(0);
-        $day->setName("lundi");
-        $this->tab_days[] = $day;
-        $day = new Day();
-        $day->setId(1);
-        $day->setName("mardi");
-        $this->tab_days[] = $day;
-        $day = new Day();
-        $day->setId(2);
-        $day->setName("mercredi");
-        $this->tab_days[] = $day;
-        $day = new Day();
-        $day->setId(3);
-        $day->setName("jeudi");
-        $this->tab_days[] = $day;
-        $day = new Day();
-        $day->setId(4);
-        $day->setName("vendredi");
-        $this->tab_days[] = $day;
-        $day = new Day();
-        $day->setId(5);
-        $day->setName("samedi");
-        $this->tab_days[] = $day;
-        $this->description = "";
-        $this->id = "";
-        $this->tag = "";
-        $this->date = "";
+        $monday = new Day();
+        $monday->setId(0);
+        $monday->setName("lundi");
+        $this->tab_days[] = $monday;
+        $tuesday = new Day();
+        $tuesday->setId(1);
+        $tuesday->setName("mardi");
+        $this->tab_days[] = $tuesday;
+        $wednesday = new Day();
+        $wednesday->setId(2);
+        $wednesday->setName("mercredi");
+        $this->tab_days[] = $wednesday;
+        $thursday = new Day();
+        $thursday->setId(3);
+        $thursday->setName("jeudi");
+        $this->tab_days[] = $thursday;
+        $friday = new Day();
+        $friday->setId(4);
+        $friday->setName("vendredi");
+        $this->tab_days[] = $friday;
+        $saturday = new Day();
+        $saturday->setId(5);
+        $saturday->setName("samedi");
+        $this->tab_days[] = $saturday;
+        $sunday = new Day();
+        $sunday->setId(6);
+        $sunday->setName("dimanche");
+        $this->tab_days[] = $sunday;
     }
-    
+
     public function getTab_days() {
         return $this->tab_days;
     }
@@ -83,37 +81,34 @@ class Week
     public function setDate($date) {
         $this->date = $date;
     }
-    
-    public function addDay($day)
-    {
+
+    public function addDay($day) {
         $this->tab_days[] = $day;
     }
-    
-    public function getDayById($day_id)
-    {
-        foreach ($this->tab_days as $day)
-        {
-            if($day->getId() == $day_id)
+
+    public function getDayById($day_id) {
+        foreach ($this->tab_days as $day) {
+            if ($day->getId() == $day_id) {
                 return $day;
+            }
         }
         return null;
     }
-    
+
     /**
      * 
      * @param type $day_name
      * @return type
      */
-    public function getDayByName($day_name)
-    {
-        foreach ($this->tab_days as $day)
-        {
-            if($day->getName() == $day_name)
+    public function getDayByName($day_name) {
+        foreach ($this->tab_days as $day) {
+            if ($day->getName() == $day_name) {
                 return $day;
+            }
         }
         return null;
     }
-    
+
     /**
      * 
      * @param type $start_time
@@ -121,16 +116,13 @@ class Week
      * @param type $formation_id
      * @return type
      */
-    public function getWeekFreeEventsList($start_time, $end_time, $formation_id)
-    {
+    public function getWeekFreeEventsList($start_time, $end_time, $formation_id) {
         $tab_free_events = array();
-        foreach ($this->tab_days as $day)
-        {
+        foreach ($this->tab_days as $day) {
             //if(count($day->getTab_events()) > 0)
-                $tab_free_events[$day->getName()] = $day->getFreeEventsList($start_time, $end_time, $formation_id);
+            $tab_free_events[$day->getName()] = $day->getFreeEventsList($start_time, $end_time, $formation_id);
         }
         return $tab_free_events;
     }
-
 
 }
