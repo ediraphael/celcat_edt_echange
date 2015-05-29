352333455225591783140d41d79b6352c8d7add7
raphael <raphael.pillie@gmail.com>
2015-05-29 15:55:23 +0200

    Ajout d'un fichier js pour le nouveau controller

---
+++ CelcatManager/src/CelcatManagement/AppBundle/Resources/public/js/index-calendar-settings.js
@@ -0,0 +1,53 @@
+$(function () {
+    var date = new Date();
+    var d = date.getDate();
+    var m = date.getMonth();
+    var y = date.getFullYear();
+
+    $('#calendar-holder').fullCalendar({
+        header: {
+            left: 'prev, next',
+            center: 'title',
+            right: ''
+        },
+        defaultView: 'agendaWeek',
+        allDaySlot: true,
+        slotDuration: '00:30:00',
+        axisFormat: 'HH:mm',
+        maxTime: '21:00',
+        minTime: '7:00',
+        dragOpacity: {
+            agenda: .5
+        },
+        editable: false,
+
+        timeFormat: {
+            // for agendaWeek and agendaDay
+            agenda: 'h:mm', // 5:00 - 6:30
+
+            // for all other views
+            '': 'h:mm'         // 7p
+        },
+        eventSources: [
+            {
+                url: Routing.generate('fullcalendar_loader'),
+                type: 'POST',
+                // A way to add custom filters to your event listeners
+                data: {
+                    calendar: ''
+                },
+                error: function () {
+                    //   alert('There was an error while fetching Google Calendar!');
+                }
+            }
+        ],
+        eventClick: function(calEvent, jsEvent, view) {
+//            alert('Event: ' + calEvent.title);
+//          console.log(calEvent);
+            $(this).css('border-color','red');
+            calEvent.editable = !calEvent.editable;
+            
+            
+        }
+    });
+});
