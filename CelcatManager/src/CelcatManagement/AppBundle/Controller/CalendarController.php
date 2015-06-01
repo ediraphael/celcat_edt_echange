7c23bcbf9efa9d0c3ceca3732bf465ac0146849e
etudiant <etudiant@localhost.localdomain>
2015-06-01 10:05:22 +0200

    ajout d'une méthode dans le contrôleur

--- CelcatManager/src/CelcatManagement/AppBundle/Controller/CalendarController.php
+++ CelcatManager/src/CelcatManagement/AppBundle/Controller/CalendarController.php
@@ -37,4 +37,27 @@ class CalendarController extends Controller
         
         return $response;
     }
+    
+    public function loadEventCalendarAction(Request $request)
+    {
+        $startDatetime = new \DateTime($request->request->get('start'));        
+        $endDatetime = new \DateTime($request->request->get('end'));
+        $urlPath = $this->container->getParameter('celcat.url').$this->container->getParameter('celcat.studentPath').$request->request->get('calendar');
+        $request->request->add(array('urlPath' => $urlPath));
+        $events = $this->container->get('event_dispatcher')->dispatch(CalendarEvent::CONFIGURE, new CalendarEvent($startDatetime, $endDatetime, $request))->getEvents();
+        
+        $response = new \Symfony\Component\HttpFoundation\Response();
+        $response->headers->set('Content-Type', 'application/json');
+        
+        $return_events = array();
+        
+        foreach($events as $event) {
+            $return_events[] = $event->toArray();    
+        }
+        
+        
+        $response->setContent(json_encode($return_events));
+        
+        return $response;
+    }
 }
