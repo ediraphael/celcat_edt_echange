f4a41d627a917d4b02f44e56948a55df4e7ce6c1
raphael <raphael.pillie@gmail.com>
2015-06-03 17:00:51 +0200

    Ajout d'une class Email

---
+++ CelcatManager/src/CelcatManagement/AppBundle/Email/Email.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace CelcatManagement\AppBundle\Email;
+
+/**
+ * Description of Email
+ *
+ * @author raphael
+ */
+class Email {
+
+    /**
+     * From adress
+     * @var string 
+     */
+    private $fromAddress;
+
+    /**
+     * To adress
+     * @var string
+     */
+    private $toAddress;
+
+    /**
+     * Subject
+     * @var string 
+     */
+    private $subject;
+
+    /**
+     * Body
+     * @var string 
+     */
+    private $body;
+
+    public function getFromAddress() {
+        return $this->fromAddress;
+    }
+
+    public function getToAddress() {
+        return $this->toAddress;
+    }
+
+    public function getSubject() {
+        return $this->subject;
+    }
+
+    public function getBody() {
+        return $this->body;
+    }
+
+    public function setFromAddress($fromAddress) {
+        $this->fromAddress = $fromAddress;
+        return $this;
+    }
+
+    public function setToAddress($toAddress) {
+        $this->toAddress = $toAddress;
+        return $this;
+    }
+
+    public function setSubject($subject) {
+        $this->subject = $subject;
+        return $this;
+    }
+
+    public function setBody($body) {
+        $this->body = $body;
+        return $this;
+    }
+
+}
