700d0399931ec4f80b398f27c23f88617594dd57
raphael <raphael.pillie@gmail.com>
2015-06-03 18:07:33 +0200

    Prise en compte du nouveau fonctionnement celcat

--- CelcatManager/src/CelcatManagement/AppBundle/Controller/DefaultController.php
+++ CelcatManager/src/CelcatManagement/AppBundle/Controller/DefaultController.php
@@ -9,10 +9,15 @@ class DefaultController extends Controller {
     public function indexAction() {
         $user = $this->getUser();
         /* @var $user \CelcatManagement\AppBundle\Security\User */
-        $url = $this->container->getParameter('celcat.url').$this->container->getParameter('celcat.teacherPath');
-        $calendarFileSource = $url.$user->getIdentifier().'.xml';
+        $url = $this->container->getParameter('celcat.url') . $this->container->getParameter('celcat.teacherPath') . $this->container->getParameter('celcat.teacherFileGet');
+        $argument = '?' . $this->container->getParameter('celcat.teacherFileArgumentId') . '=' . $user->getIdentifier() .
+                '&' . $this->container->getParameter('celcat.teacherFileArgumentKey') . '=' . mktime(0, 0, 0, date("m"), date("d"), date("Y"));
+
+
+
+        $calendarFileSource = $url . $argument;
         return $this->render('CelcatManagementAppBundle:Default:index.html.twig', array(
-            'calendar_file_source' => $calendarFileSource
+                    'calendar_file_source' => $calendarFileSource
         ));
     }
 
