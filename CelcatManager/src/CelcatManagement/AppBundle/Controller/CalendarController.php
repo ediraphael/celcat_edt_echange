052e71f3e2558848383c999637fb47d3f49ea0e5
raphael <raphael.pillie@gmail.com>
2015-06-03 17:44:24 +0200

    Modification du controller calendar pour verifier sur le calendrier fourni n'est pas déja une URL

--- CelcatManager/src/CelcatManagement/AppBundle/Controller/CalendarController.php
+++ CelcatManager/src/CelcatManagement/AppBundle/Controller/CalendarController.php
@@ -4,90 +4,88 @@ namespace CelcatManagement\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-
 use CelcatManagement\AppBundle\Event\CalendarEvent;
 
-class CalendarController extends Controller
-{
+class CalendarController extends Controller {
+
     public function indexAction(Request $request) {
         $groupManager = new \CelcatManagement\CelcatReaderBundle\Models\GroupManager();
-        $url = $this->container->getParameter('celcat.url').$this->container->getParameter('celcat.studentPath').$this->container->getParameter('celcat.groupIndex');
+        $url = $this->container->getParameter('celcat.url') . $this->container->getParameter('celcat.studentPath') . $this->container->getParameter('celcat.groupIndex');
+        //$url = $this->container->getParameter('celcat.url').$this->container->getParameter('celcat.teacherPath').$this->container->getParameter('celcat.personnelIndex');
+        echo $url;
         $groupManager->loadGroups($url);
-        
+
         return $this->render('CelcatManagementAppBundle:Calendar:index.html.twig', array('groupList' => $groupManager->getGroupList()));
     }
-    
+
     /**
      * Dispatch a CalendarEvent and return a JSON Response of any events returned.
      * 
      * @param Request $request
      * @return Response
      */
-    public function loadCalendarAction(Request $request)
-    {
-        $startDatetime = new \DateTime($request->request->get('start'));        
+    public function loadCalendarAction(Request $request) {
+        $startDatetime = new \DateTime($request->request->get('start'));
         $endDatetime = new \DateTime($request->request->get('end'));
-        $urlPath = $this->container->getParameter('celcat.url').$this->container->getParameter('celcat.studentPath').$request->request->get('calendar');
+        $urlPath = $request->request->get('calendar');
+        if ((substr($urlPath, 0, 4) !== 'http')) {
+            $urlPath = $this->container->getParameter('celcat.url') . $this->container->getParameter('celcat.studentPath') . $urlPath;
+        }
         $request->request->add(array('urlPath' => $urlPath));
         $events = $this->container->get('event_dispatcher')->dispatch(CalendarEvent::CONFIGURE, new CalendarEvent($startDatetime, $endDatetime, $request))->getEvents();
-        
+
         $response = new \Symfony\Component\HttpFoundation\Response();
         $response->headers->set('Content-Type', 'application/json');
-        
+
         $return_events = array();
-        
-        foreach($events as $event) {
-            $return_events[] = $event->toArray();    
+
+        foreach ($events as $event) {
+            $return_events[] = $event->toArray();
         }
-        
-        
+
+
         $response->setContent(json_encode($return_events));
-        
+
         return $response;
     }
-    
-    public function loadEventCalendarAction(Request $request)
-    {
-        $startDatetime = new \DateTime($request->request->get('start'));        
+
+    public function loadEventCalendarAction(Request $request) {
+        $startDatetime = new \DateTime($request->request->get('start'));
         $endDatetime = new \DateTime($request->request->get('end'));
-        $urlPath = $this->container->getParameter('celcat.url').$this->container->getParameter('celcat.studentPath').$request->request->get('calendar');
+        $urlPath = $this->container->getParameter('celcat.url') . $this->container->getParameter('celcat.studentPath') . $request->request->get('calendar');
         $request->request->add(array('urlPath' => $urlPath));
         $events = $this->container->get('event_dispatcher')->dispatch(CalendarEvent::CONFIGURE, new CalendarEvent($startDatetime, $endDatetime, $request))->getEvents();
-        
+
         $response = new \Symfony\Component\HttpFoundation\Response();
         $response->headers->set('Content-Type', 'application/json');
-        
+
         $return_events = array();
         $current_user = $this->getUser();
-        foreach($events as $event) 
-        {
-            if($current_user->calendarExists($event->getFormation()))
-            {
-                $return_events[$event->getId()] = $event->toArray();    
+        foreach ($events as $event) {
+            if ($current_user->calendarExists($event->getFormation())) {
+                $return_events[$event->getId()] = $event->toArray();
             }
         }
-        
-        
+
+
         $response->setContent(json_encode($return_events));
-        
+
         return $response;
     }
-      
-    public function swapTwoEventsAction(Request $request)
-    {
+
+    public function swapTwoEventsAction(Request $request) {
         $schedulerManager = unserialize($_SESSION['schedulerManager']);
         $obj_event_source = $request->request->get('event_source');
         $obj_event_destination = $request->request->get('event_destination');
         $event_source = $schedulerManager->getWeekByTag($obj_event_source['week'])->getDayById($obj_event_source['day'])->getEventByIdAndByFormation($obj_event_source['id'], $obj_event_source['formation']);
         $event_destination = $schedulerManager->getWeekByTag($obj_event_destination['week'])->getDayById($obj_event_destination['day'])->getEventByIdAndByFormation($obj_event_destination['id'], $obj_event_destination['formation']);
-        
+
         $result = $schedulerManager->canSwapEvent($event_source, $event_destination);
-        
+
         $response = new \Symfony\Component\HttpFoundation\Response();
         $response->headers->set('Content-Type', 'application/json');
         $response->setContent(json_encode($result));
         return $response;
     }
-    
-    
+
 }
