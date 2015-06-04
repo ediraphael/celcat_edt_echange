7bfa4f041e7576bc8c292d041b0b362b2d5eed07
raphael <raphael.pillie@gmail.com>
2015-06-04 19:01:24 +0200

    Modification du calendar Event listener pour prendre en compte le flag deleted de l'event

--- CelcatManager/src/CelcatManagement/AppBundle/EventListener/CalendarEventListener.php
+++ CelcatManager/src/CelcatManagement/AppBundle/EventListener/CalendarEventListener.php
@@ -6,45 +6,44 @@ use CelcatManagement\AppBundle\Event\CalendarEvent;
 use ADesigns\CalendarBundle\Entity\EventEntity;
 use Doctrine\ORM\EntityManager;
 
-class CalendarEventListener
-{
+class CalendarEventListener {
+
     private $schedulerManager;
-    public function __construct()
-    {
+
+    public function __construct() {
         $this->schedulerManager = new \CelcatManagement\CelcatReaderBundle\Models\ScheduleManager();
     }
 
-    public function loadEvents(CalendarEvent $calendarEvent)
-    {
+    public function loadEvents(CalendarEvent $calendarEvent) {
         $startDate = $calendarEvent->getStartDatetime();
         $endDate = $calendarEvent->getEndDatetime();
         $url = $calendarEvent->getRequest()->request->get('urlPath');
-        
+
         $this->schedulerManager->parseAllSchedule($url);
         $arrayWeeks = $this->schedulerManager->getArrayWeeks();
-        
+
         foreach ($arrayWeeks as $indexWeek => $week) {
             foreach ($week->getArrayDays() as $indexDay => $day) {
-                foreach ($day->getArrayEvents() as $indexEvents => $events) {
-                    foreach ($events as $groupeName => $event) {
+                foreach ($day->getArrayEvents() as $indexEvents => $event) {
+                    if (!$event->isDeleted()) {
                         $calendarEvent->addEvent($event);
                     }
                 }
             }
         }
     }
-    
-    public function refreshEvents(CalendarEvent $calendarEvent)
-    {
+
+    public function refreshEvents(CalendarEvent $calendarEvent) {
         $arrayWeeks = $this->schedulerManager->getArrayWeeks();
         foreach ($arrayWeeks as $indexWeek => $week) {
             foreach ($week->getArrayDays() as $indexDay => $day) {
-                foreach ($day->getArrayEvents() as $indexEvents => $events) {
-                    foreach ($events as $groupeName => $event) {
+                foreach ($day->getArrayEvents() as $indexEvents => $event) {
+                    if (!$event->isDeleted()) {
                         $calendarEvent->addEvent($event);
                     }
                 }
             }
         }
     }
+
 }
