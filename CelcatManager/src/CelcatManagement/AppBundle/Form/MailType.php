d5f1def0c5cb1278c4862c6f02b204703e23f792
raphael <raphael.pillie@gmail.com>
2015-06-04 15:47:53 +0200

    Renomage d'un formulaire

---
+++ CelcatManager/src/CelcatManagement/AppBundle/Form/MailType.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace CelcatManagement\AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class MailType extends AbstractType {
+
+    /**
+     * Pre fix fromAddress
+     * @var string 
+     */
+    private $fromAddress;
+
+    /**
+     * Pre fix toAddress
+     * @var string 
+     */
+    private $toAddress;
+
+    public function __construct($fromAddress = '', $toAddress = '') {
+        $this->fromAddress = $fromAddress;
+        $this->toAddress = $toAddress;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        if ($this->fromAddress != '') {
+            $builder
+                    ->add('fromAddress', 'email', array(
+                        'label' => 'De',
+                        'data' => $this->fromAddress,
+                        'read_only' => true,
+                        'widget_addon_prepend' => array(
+                            'text' => '@',
+                        )
+            ));
+        }
+        else {
+            $builder
+                    ->add('fromAddress', 'email', array(
+                        'label' => 'De',
+                        'widget_addon_prepend' => array(
+                            'text' => '@',
+                        )
+            ));
+        }
+        if ($this->toAddress != '') {
+            $builder
+                    ->add('toAddress', 'email', array(
+                        'label' => 'De',
+                        'data' => $this->toAddress,
+                        'read_only' => true,
+                        'widget_addon_prepend' => array(
+                            'text' => '@',
+                        )
+            ));
+        }
+        else {
+            $builder
+                    ->add('toAddress', 'email', array(
+                        'label' => 'De',
+                        'widget_addon_prepend' => array(
+                            'text' => '@',
+                        )
+            ));
+        }
+        $builder
+                ->add('subject', 'text', array(
+                    'label' => 'sujet',
+                ))
+                ->add('body', 'ckeditor', array(
+                    'attr' => array('rows' => 20),
+                    'label' => 'message',
+                ))
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
+        
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName() {
+        return 'mailer_email';
+    }
+
+}
