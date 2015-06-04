67cb7189377270bcfc8a39104cefc3319e072001
raphael <raphael.pillie@gmail.com>
2015-06-04 15:54:36 +0200

    Modificaton de la class UserMail type pour recpercuter le renomage de mailType

--- CelcatManager/src/CelcatManagement/AppBundle/Form/UserMailType.php
+++ CelcatManager/src/CelcatManagement/AppBundle/Form/UserMailType.php
@@ -6,7 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class UserMailType extends EmailType
+class UserMailType extends MailType
 {
     /**
      * @param OptionsResolverInterface $resolver
