c18ca0d93f762ca9b26b00892786d510338b3b1f
raphael <raphael.pillie@gmail.com>
2015-06-04 15:55:24 +0200

    Ajout d'un formulaire pour les models de mails

---
+++ CelcatManager/src/CelcatManagement/AppBundle/Form/ModelMailType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace CelcatManagement\AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ModelMailType extends MailType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+        ;
+        parent::buildForm($builder, $options);
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'CelcatManagement\AppBundle\Entity\ModelMail'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'celcatmanagement_appbundle_modelmail';
+    }
+}
