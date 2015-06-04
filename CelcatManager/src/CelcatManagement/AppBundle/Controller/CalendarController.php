4b42adaf253c1e147b171c722cb8b8d57ff18b1f
etudiant <etudiant@localhost.localdomain>
2015-06-04 17:23:02 +0200

    ajout de méthode pour le swap de deux evennement et de rafraichissement du calendrier avec le singleton schedulerManager

--- CelcatManager/src/CelcatManagement/AppBundle/Controller/CalendarController.php
+++ CelcatManager/src/CelcatManagement/AppBundle/Controller/CalendarController.php
@@ -46,6 +46,33 @@ class CalendarController extends Controller
         return $response;
     }
     
+    /**
+     * Dispatch a CalendarEvent and return a JSON Response of any events returned.
+     * 
+     * @param Request $request
+     * @return Response
+     */
+    public function refreshCalendarAction(Request $request)
+    {
+        $startDatetime = new \DateTime($request->request->get('start'));        
+        $endDatetime = new \DateTime($request->request->get('end'));
+        $events = $this->container->get('event_dispatcher')->dispatch(CalendarEvent::CONFIGURE_REFRESH, new CalendarEvent($startDatetime, $endDatetime, $request))->getEvents();
+        
+        $response = new \Symfony\Component\HttpFoundation\Response();
+        $response->headers->set('Content-Type', 'application/json');
+        
+        $return_events = array();
+        
+        foreach($events as $event) {
+            $return_events[] = $event->toArray();    
+        }
+        
+        
+        $response->setContent(json_encode($return_events));
+        
+        return $response;
+    }
+    
     public function loadEventCalendarAction(Request $request)
     {
         $startDatetime = new \DateTime($request->request->get('start'));        
@@ -73,16 +100,36 @@ class CalendarController extends Controller
         return $response;
     }
       
+    public function canSwapTwoEventsAction(Request $request)
+    {
+        $schedulerManager = new \CelcatManagement\CelcatReaderBundle\Models\ScheduleManager();
+        $obj_event_source = $request->request->get('event_source');
+        $obj_events_destination = $request->request->get('events_destination');
+
+        $event_source = $schedulerManager->getWeekByTag($obj_event_source['week'])->getDayById($obj_event_source['day'])->getEventByIdAndByFormation($obj_event_source['id'], $obj_event_source['formation']);
+        $result = array();
+        foreach ($obj_events_destination as $obj_event_destination)
+        {
+            $event_destination = $schedulerManager->getWeekByTag($obj_event_destination['week'])->getDayById($obj_event_destination['day'])->getEventByIdAndByFormation($obj_event_destination['id'], $obj_event_destination['formation']);
+            $result[] = array("id" => $event_destination->getId(), "result" => $schedulerManager->canSwapEvent($event_source, $event_destination));
+        }
+        
+        $response = new \Symfony\Component\HttpFoundation\Response();
+        $response->headers->set('Content-Type', 'application/json');
+        $response->setContent(json_encode($result));
+        return $response;
+    }
+    
     public function swapTwoEventsAction(Request $request)
     {
-        $schedulerManager = unserialize($_SESSION['schedulerManager']);
+        $schedulerManager = new \CelcatManagement\CelcatReaderBundle\Models\ScheduleManager();
         $obj_event_source = $request->request->get('event_source');
         $obj_event_destination = $request->request->get('event_destination');
         $event_source = $schedulerManager->getWeekByTag($obj_event_source['week'])->getDayById($obj_event_source['day'])->getEventByIdAndByFormation($obj_event_source['id'], $obj_event_source['formation']);
         $event_destination = $schedulerManager->getWeekByTag($obj_event_destination['week'])->getDayById($obj_event_destination['day'])->getEventByIdAndByFormation($obj_event_destination['id'], $obj_event_destination['formation']);
         
-        $result = $schedulerManager->canSwapEvent($event_source, $event_destination);
-        
+        $result = $schedulerManager->swapEvent($event_source, $event_destination);
+       
         $response = new \Symfony\Component\HttpFoundation\Response();
         $response->headers->set('Content-Type', 'application/json');
         $response->setContent(json_encode($result));
