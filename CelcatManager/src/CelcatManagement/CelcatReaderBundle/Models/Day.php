03c2fd9ac676f525366d5255a2e8cdf759126895
raphael <raphael.pillie@gmail.com>
2015-06-04 18:47:35 +0200

    MOdification de la class Day suite a celle de la class Event

--- CelcatManager/src/CelcatManagement/CelcatReaderBundle/Models/Day.php
+++ CelcatManager/src/CelcatManagement/CelcatReaderBundle/Models/Day.php
@@ -28,8 +28,8 @@ class Day {
         return $this->name;
     }
 
-    public function setArrayEvents($tab_events) {
-        $this->arrayEvents = $tab_events;
+    public function setArrayEvents($arrayEvents) {
+        $this->arrayEvents = $arrayEvents;
     }
 
     public function setId($id) {
@@ -39,237 +39,214 @@ class Day {
     public function setName($name) {
         $this->name = $name;
     }
-    
 
     /**
      * 
-     * @param type $event_id
-     * @param type $formation_id
-     * @return type
+     * @param string $eventId
+     * @param string $formationId
+     * @return Event|null
      */
-    public function getEventByIdAndByFormation($event_id, $formation_id) {
-        if (isset($this->arrayEvents[$formation_id]))
-        {
-            foreach ($this->arrayEvents[$formation_id] as $event) {
-                if ($event->getId() == $event_id) {
-                    return $event;
-                }
+    public function getEventByIdAndByFormation($eventId, $formationId) {
+        foreach ($this->arrayEvents as $event) {
+            if ($event->getId() == $eventId && $event->getFormations()->contains($formationId)) {
+                return $event;
             }
         }
         return null;
     }
-    
-    
-    /**
-     * 
-     * @param type $event_id
-     * @return type
-     */
-    public function getEventByIdInAllFormations($event_id) {
-        foreach ($this->arrayEvents as $events) {
-            foreach ($events as $event)
-            {
-                if ($event->getId() == $event_id) {
-                    return $event;
-                }
-            }
-        }
-        return null;
-    }
-    
 
     /**
      * 
-     * @param type $start_time
-     * @param type $end_time
-     * @return type
+     * @param string $eventId
+     * @return Event|null
      */
-    private function getDifferenceOfTime($start_time, $end_time) {
-        $end = strtotime($end_time);
-        $start = strtotime($start_time);
+    public function getEventByIdInAllFormations($eventId) {
+        if (isset($this->arrayEvents[$eventId]) && $this->arrayEvents[$eventId]) {
+            return $this->arrayEvents[$eventId];
+        }
+        return null;
+    }
+
+    /**
+     * 
+     * @param string $startTime
+     * @param string $endTime
+     * @return string
+     */
+    private function getDifferenceOfTime($startTime, $endTime) {
+        $end = strtotime($endTime);
+        $start = strtotime($startTime);
         return gmdate('H:i', $end - $start);
     }
 
     /**
      * 
-     * @param type $event
+     * @param Event $event
      */
     public function addEvent($event) {
-        if($this->getEventByIdAndByFormation($event->getId(), $event->getFormation()) == null)
-        {
-            // le créneau est ajouté dans un tableau via un index qui représente l'id de la formation
-            if (!isset($this->arrayEvents[$event->getFormation()])) {
-                $this->arrayEvents[$event->getFormation()] = array();
-            }
-            array_push($this->arrayEvents[$event->getFormation()], $event);
-        }    
-    }
-    
-    
-    /**
-     * 
-     * @param type $event_id
-     * @param type $formation_id
-     * @return boolean
-     */
-    public function removeEvent($event_id, $formation_id)
-    {
-        foreach ($this->arrayEvents as $index1 => $events) {
-            foreach ($events as $index2 => $event)
-            {
-                if ($event->getId() == $event_id) {
-                    unset($this->arrayEvents[$index1][$index2]);
-                }
-            }
+        if (isset($this->arrayEvents[$event->getId()]) && $this->arrayEvents[$event->getId()] != '') {
+            $this->arrayEvents[$event->getId()]->addFormation($event->getFormations());
+        } else {
+            $this->arrayEvents[$event->getId()] = $event;
         }
-        return true;
     }
-    
 
     /**
      * 
-     * @param type $start_time
-     * @param type $end_time
-     * @param type $formation_id
+     * @param string $eventId
      * @return boolean
      */
-    public function canAddEvent($event_source_id, $start_time, $end_time, $formation_id) {
-        foreach ($this->arrayEvents[$formation_id] as $event) {
-            if($event_source_id != $event->getId())
-            {
-                if (($start_time > $event->getStartTime() && $start_time < $event->getEndTime()) ||
-                        ($end_time > $event->getStartTime() && $end_time < $event->getEndTime()) || 
-                        ($start_time < $event->getStartTime() && $end_time > $event->getEndTime())) {
-                    return false;
-                }
+    public function removeEvent($eventId) {
+        if (isset($this->arrayEvents[$eventId]) && $this->arrayEvents[$eventId]) {
+            $this->arrayEvents[$eventId]->delete();
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 
+     * @param string $eventId
+     * @param string $startTime
+     * @param string $endTime
+     * @return boolean
+     */
+    public function canAddEvent($eventId, $startTime, $endTime) {
+        if (isset($this->arrayEvents[$event_id]) && $this->arrayEvents[$event_id]) {
+            $event = $this->arrayEvents[$event_id];
+            if (($startTime > $event->getStartTime() && $startTime < $event->getEndTime()) ||
+                    ($endTime > $event->getStartTime() && $endTime < $event->getEndTime()) ||
+                    ($startTime < $event->getStartTime() && $endTime > $event->getEndTime())) {
+                return false;
             }
         }
+
         return true;
     }
 
     /**
      * 
-     * @param type $start_time
-     * @param type $end_time
+     * @param type $startTime
+     * @param type $endTime
      * @param type $formation_id
      * @return array
      */
-    public function getFreeEventsList($start_time, $end_time, $formation_id) {
-        $tab_busy_events = array();
-        $tab_free_events = array();
-        $wanted_duration = $this->getDifferenceOfTime($start_time, $end_time);
-        if (count($this->arrayEvents) > 0 && count($this->arrayEvents[$formation_id])) {
-            foreach ($this->arrayEvents[$formation_id] as $index => $event) {
-                $tab_temp = array();
-                $tab_temp[] = $event->getStartTime();
-                $tab_temp[] = $event->getEndTime();
-                $tab_busy_events[] = $tab_temp;
+    public function getFreeEventsList($startTime, $endTime, $formation_id) {
+        $tabBusEvents = array();
+        $tabFreeEvents = array();
+        $wantedDuration = $this->getDifferenceOfTime($startTime, $endTime);
+        if (count($this->arrayEvents) > 0) {
+            foreach ($this->arrayEvents as $index => $event) {
+                $tabTemp = array();
+                $tabTemp[] = $event->getStartTime();
+                $tabTemp[] = $event->getEndTime();
+                $tabBusEvents[] = $tabTemp;
             }
         }
-        if (count($tab_busy_events) == 0) {
-            $this->insertFreeEvent($tab_free_events, "08:00", "20:00", $wanted_duration);
+        if (count($tabBusEvents) == 0) {
+            $this->insertFreeEvent($tabFreeEvents, "08:00", "20:00", $wantedDuration);
         } else {
             //        return $tab_busy_events;
-            foreach ($tab_busy_events as $index => $tab_time) {
+            foreach ($tabBusEvents as $index => $tabTime) {
                 $var_start_time = null;
                 $var_end_time = null;
-                $free_duration_between_two_events = $this->getDifferenceOfTime($tab_time[0], $tab_time[1]);
-                if ($index == 0 && $this->getDifferenceOfTime("08:00", $tab_time[0]) >= $wanted_duration) {
+                $free_duration_between_two_events = $this->getDifferenceOfTime($tabTime[0], $tabTime[1]);
+                if ($index == 0 && $this->getDifferenceOfTime("08:00", $tabTime[0]) >= $wantedDuration) {
                     $var_start_time = "08:00";
-                    $var_end_time = $tab_time[0];
-                    $this->insertFreeEvent($tab_free_events, $var_start_time, $var_end_time, $wanted_duration);
+                    $var_end_time = $tabTime[0];
+                    $this->insertFreeEvent($tabFreeEvents, $var_start_time, $var_end_time, $wantedDuration);
                 }
-                if (count($tab_busy_events) - 1 > $index) {
-                    $next_tab_time = $tab_busy_events[$index + 1];
+                if (count($tabBusEvents) - 1 > $index) {
+                    $next_tab_time = $tabBusEvents[$index + 1];
                     //                dans le cas ou les créneaux se chevauche (plusieurs créneaux en même temps)
-                    if ($tab_time[1] >= $next_tab_time[0] || $this->getDifferenceOfTime($tab_time[1], "20:00") < $wanted_duration) {
+                    if ($tabTime[1] >= $next_tab_time[0] || $this->getDifferenceOfTime($tabTime[1], "20:00") < $wantedDuration) {
                         continue;
                     }
-                    $free_duration_between_two_events = $this->getDifferenceOfTime($tab_time[1], $next_tab_time[0]);
-                    if ($free_duration_between_two_events >= $wanted_duration) {
-                        $var_start_time = $tab_time[1];
+                    $free_duration_between_two_events = $this->getDifferenceOfTime($tabTime[1], $next_tab_time[0]);
+                    if ($free_duration_between_two_events >= $wantedDuration) {
+                        $var_start_time = $tabTime[1];
                         $var_end_time = $next_tab_time[0];
                     }
                 } else {
-                    if ($this->getDifferenceOfTime($tab_time[1], "20:00") >= $wanted_duration) {
-                        $var_start_time = $tab_time[1];
+                    if ($this->getDifferenceOfTime($tabTime[1], "20:00") >= $wantedDuration) {
+                        $var_start_time = $tabTime[1];
                         $var_end_time = "20:00";
                     }
                 }
-                $this->insertFreeEvent($tab_free_events, $var_start_time, $var_end_time, $wanted_duration);
+                $this->insertFreeEvent($tabFreeEvents, $var_start_time, $var_end_time, $wantedDuration);
             }
         }
-        $this->getSubEvents($tab_free_events, $wanted_duration);
-        return $tab_free_events;
+        $this->getSubEvents($tabFreeEvents, $wantedDuration);
+        return $tabFreeEvents;
     }
 
     /**
      * 
-     * @param type $tab_free_events
-     * @param type $var_start_time
-     * @param type $var_end_time
-     * @param type $wanted_duration
+     * @param type $tabFreeEvents
+     * @param type $varStartTime
+     * @param type $varEndTime
+     * @param type $wantedDuration
      */
-    private function insertFreeEvent(&$tab_free_events, $var_start_time, $var_end_time, $wanted_duration) {
-        if ($var_start_time != null && $var_end_time != null) {
-            $temp_tab_free_duration = array();
-            if ($var_end_time > "12:30" && $var_start_time < "12:30") {
-                if ($this->getDifferenceOfTime($var_start_time, "12:30") >= $wanted_duration) {
-                    $temp_tab_free_duration[] = $var_start_time;
-                    $temp_tab_free_duration[] = "12:30";
-                    $tab_free_events[] = $temp_tab_free_duration;
+    private function insertFreeEvent(&$tabFreeEvents, $varStartTime, $varEndTime, $wantedDuration) {
+        if ($varStartTime != null && $varEndTime != null) {
+            $tempTabFreeDuration = array();
+            if ($varEndTime > "12:30" && $varStartTime < "12:30") {
+                if ($this->getDifferenceOfTime($varStartTime, "12:30") >= $wantedDuration) {
+                    $tempTabFreeDuration[] = $varStartTime;
+                    $tempTabFreeDuration[] = "12:30";
+                    $tabFreeEvents[] = $tempTabFreeDuration;
                 }
-                if ($this->getDifferenceOfTime("13:30", $var_end_time) >= $wanted_duration && $var_end_time > "13:30") {
-                    $temp_tab_free_duration = array();
-                    $temp_tab_free_duration[] = "13:30";
-                    $temp_tab_free_duration[] = $var_end_time;
-                    $tab_free_events[] = $temp_tab_free_duration;
+                if ($this->getDifferenceOfTime("13:30", $varEndTime) >= $wantedDuration && $varEndTime > "13:30") {
+                    $tempTabFreeDuration = array();
+                    $tempTabFreeDuration[] = "13:30";
+                    $tempTabFreeDuration[] = $varEndTime;
+                    $tabFreeEvents[] = $tempTabFreeDuration;
                 }
             } else {
-                $temp_tab_free_duration[] = $var_start_time;
-                $temp_tab_free_duration[] = $var_end_time;
-                $tab_free_events[] = $temp_tab_free_duration;
+                $tempTabFreeDuration[] = $varStartTime;
+                $tempTabFreeDuration[] = $varEndTime;
+                $tabFreeEvents[] = $tempTabFreeDuration;
             }
         }
     }
 
     /**
      * 
-     * @param type $tab_free_events
-     * @param type $wanted_duration
+     * @param type $tabFreeEvents
+     * @param type $wantedDuration
      */
-    private function getSubEvents(&$tab_free_events, $wanted_duration) {
+    private function getSubEvents(&$tabFreeEvents, $wantedDuration) {
         // $desired_additionel_time = "+1 hours";
-        $desired_additionel_time = "+15 minutes";
-        $temp_tab_free_events = array();
-        foreach ($tab_free_events as $interval) {
-            if ($this->getDifferenceOfTime($interval[0], $interval[1]) > $wanted_duration) {
+        $desiredAdditionelTime = "+15 minutes";
+        $tempTabFreeEvents = array();
+        foreach ($tabFreeEvents as $interval) {
+            if ($this->getDifferenceOfTime($interval[0], $interval[1]) > $wantedDuration) {
                 $tab_temp = array();
-                $hours = explode(":", $wanted_duration)[0];
-                $minutes = explode(":", $wanted_duration)[1];
+                $hours = explode(":", $wantedDuration)[0];
+                $minutes = explode(":", $wantedDuration)[1];
                 $convert = strtotime("+$hours hours", strtotime($interval[0]));
                 $convert = strtotime("+$minutes minutes", $convert);
                 $calculated = date('H:i', $convert);
-                $this->insertFreeEvent($temp_tab_free_events, $interval[0], $calculated, $wanted_duration);
-                $calculated = date('H:i', strtotime($desired_additionel_time, strtotime($interval[0])));
+                $this->insertFreeEvent($tempTabFreeEvents, $interval[0], $calculated, $wantedDuration);
+                $calculated = date('H:i', strtotime($desiredAdditionelTime, strtotime($interval[0])));
                 while ($calculated < $interval[1]) {
-                    $old_calculated = $calculated;
-                    $hours = explode(":", $wanted_duration)[0];
-                    $minutes = explode(":", $wanted_duration)[1];
+                    $oldCalculated = $calculated;
+                    $hours = explode(":", $wantedDuration)[0];
+                    $minutes = explode(":", $wantedDuration)[1];
                     $convert = strtotime("+$hours hours", strtotime($calculated));
                     $convert = strtotime("+$minutes minutes", $convert);
                     $calculated = date('H:i', $convert);
                     if ($calculated > "20:00") {
                         break;
                     }
-                    $this->insertFreeEvent($temp_tab_free_events, $old_calculated, $calculated, $wanted_duration);
-                    $calculated = date('H:i', strtotime($desired_additionel_time, strtotime($old_calculated)));
+                    $this->insertFreeEvent($tempTabFreeEvents, $oldCalculated, $calculated, $wantedDuration);
+                    $calculated = date('H:i', strtotime($desiredAdditionelTime, strtotime($oldCalculated)));
                 }
             } else {
-                $this->insertFreeEvent($temp_tab_free_events, $interval[0], $interval[1], $wanted_duration);
+                $this->insertFreeEvent($tempTabFreeEvents, $interval[0], $interval[1], $wantedDuration);
             }
         }
-        $tab_free_events = $temp_tab_free_events;
+        $tabFreeEvents = $tempTabFreeEvents;
     }
 
 }
