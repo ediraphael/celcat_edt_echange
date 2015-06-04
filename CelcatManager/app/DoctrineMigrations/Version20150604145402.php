72d588f26f2d703652f2680786cc421b29c8337d
raphael <raphael.pillie@gmail.com>
2015-06-04 15:11:03 +0200

    AJout d'un fichier de migration

---
+++ CelcatManager/app/DoctrineMigrations/Version20150604145402.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20150604145402 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user_mail ADD from_address VARCHAR(70) NOT NULL, ADD to_address VARCHAR(70) NOT NULL, ADD subject VARCHAR(255) NOT NULL, ADD body LONGTEXT NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user_mail DROP from_address, DROP to_address, DROP subject, DROP body');
+    }
+}
