e836dc4a0c31b83371fd416fbf08a9371e7aa9b1
etudiant <etudiant@localhost.localdomain>
2015-06-04 17:25:13 +0200

    restructuration du code de l'index-calendar-settings + optimisation de canswap

--- CelcatManager/src/CelcatManagement/AppBundle/Resources/public/js/index-calendar-settings.js
+++ CelcatManager/src/CelcatManagement/AppBundle/Resources/public/js/index-calendar-settings.js
@@ -6,6 +6,12 @@ function loadCalendarEvents(objet) {
     addCalendarEventSource(file);
 }
 
+
+function refreshCalendarEvents() {
+    removeCalendarEvents();
+    refreshCalendarEventSource();
+}
+
 function addCalendarEventSource(calendarFile) {
     if (typeof calendarFile !== 'undefined') {
         eventSources =
@@ -25,12 +31,35 @@ function addCalendarEventSource(calendarFile) {
     }
 }
 
+function refreshCalendarEventSource() {
+    eventSources =
+            {
+                url: Routing.generate('fullcalendar_refresh'),
+                type: 'POST',
+                // A way to add custom filters to your event listeners
+                data: {
+ 
+                },
+                error: function () {
+                    //   alert('There was an error while fetching Google Calendar!');
+                }
+            }
+    ;
+    $('#calendar-holder').fullCalendar('addEventSource', eventSources);
+
+}
+
 function removeCalendarEvents() {
     $('#calendar-holder').fullCalendar('removeEvents');
     $('#calendar-holder').fullCalendar('removeEventSource',
-        {
-            url: Routing.generate('fullcalendar_loader')
-        }
+            {
+                url: Routing.generate('fullcalendar_loader')
+            }
+    );
+    $('#calendar-holder').fullCalendar('removeEventSource',
+            {
+                url: Routing.generate('fullcalendar_refresh')
+            }
     );
 }
 
@@ -76,52 +105,58 @@ $(function () {
 //                }
 //            }
 //        ],
-        eventClick: function(calEvent, jsEvent, view) {
+        eventClick: function (calEvent, jsEvent, view) {
             var arrayEvents = new Array();
             $(this).toggleClass("selected_event");
-            if(calEvent.color == "orange")
+            if (calEvent.color == "orange")
             {
                 calEvent.color = "";
                 calEvent.editable = false;
-                if(two_selected_events[0] != undefined && two_selected_events[0].id == calEvent.id)
-                    two_selected_events.splice(0,1);
-                if(two_selected_events[1] != undefined && two_selected_events[1].id == calEvent.id)
-                    two_selected_events.splice(1,1);
+                if (two_selected_events[0] != undefined && two_selected_events[0].id == calEvent.id)
+                    two_selected_events.splice(0, 1);
+                if (two_selected_events[1] != undefined && two_selected_events[1].id == calEvent.id)
+                    two_selected_events.splice(1, 1);
             }
             else
             {
                 calEvent.color = "orange";
                 calEvent.editable = true;
-                if(two_selected_events.length < 2)
+                if (two_selected_events.length < 2)
                     two_selected_events.push(calEvent);
-                if(two_selected_events.length < 2)
+                if (two_selected_events.length < 2)
                 {
                     //chargement des créneaux liés a la formation du créneau selectionné
                     reloadCalendarEvents(calEvent, arrayEvents);
                 }
             }
             //récupération de tous les créneaux du calendrié + vérification de la possibilité de swap
-            if(two_selected_events.length == 1 && calEvent.color == "orange")
+            if (two_selected_events.length == 1 && calEvent.color == "orange")
             {
                 markUpAlterableEvents(calEvent);
             }
             //dans le cas ou on choisi deux créneaux pour les swapé
-            else if(two_selected_events.length == 2)
+            else if (two_selected_events.length == 2)
             {
-                if(canSwapTwoEvents(two_selected_events[0], two_selected_events[1]))
+                var array_objects_events = new Array();
+                array_objects_events.push({id: two_selected_events[1].id, day: two_selected_events[1].day, week: two_selected_events[1].week, formation: two_selected_events[1].formation});
+                if (canSwapTwoEvents(two_selected_events[0], array_objects_events)[0].result)
                 {
                     if (confirm("Voulez vous vraiment échanger ces deux évennements?")) {
-                        console.log("ok");
-                    } 
+                        swapTwoEvents(two_selected_events[0], two_selected_events[1]);
+                    }
                     else {
-                        console.log("ko");
+                        calEvent.color = "";
+                        two_selected_events.splice(1, 1);
+                        two_selected_events[0].color = "";
+                        resetEventsColor(two_selected_events[0]);
+                        two_selected_events.splice(0, 1);
                     }
                 }
             }
             //dans le cas ou ont déselectionne un créneau
             else
-            { 
-                if(two_selected_events.length == 0)
+            {
+                if (two_selected_events.length == 0)
                 {
                     resetEventsColor(calEvent);
                 }
@@ -138,40 +173,40 @@ function reloadCalendarEvents(calEvent, arrayEvents)
     $.ajax({
         type: "POST",
         async: false,
-        url: Routing.generate('event_calendar_loader'),
+        url: Routing.generate('event_calendar_refresh'),
         data: {
             calendar: calEvent.formation + '.xml'
         },
-        success: function(response) 
+        success: function (response)
         {
-            for(var i=0; i<response.length; i++)
+            for (var i = 0; i < response.length; i++)
             {
                 response[i].color = "red";
                 arrayEvents.push(response[i]);
             }
         },
-        error: function(req, status, error) {
+        error: function (req, status, error) {
             console.err(error);
         }
     });
 }
 
-function canSwapTwoEvents(event_source, event_destination)
+function canSwapTwoEvents(event_source, array_events_destination)
 {
     var result = false;
     $.ajax({
         type: "POST",
         async: false,
-        url: Routing.generate('swap_two_events'),
+        url: Routing.generate('can_swap_two_events'),
         data: {
-            event_source: {id:event_source.id, day: event_source.day, week:event_source.week, formation:event_source.formation},
-            event_destination: {id:event_destination.id, day: event_destination.day, week:event_destination.week, formation:event_destination.formation}
+            event_source: {id: event_source.id, day: event_source.day, week: event_source.week, formation: event_source.formation},
+            events_destination: array_events_destination
         },
-        success: function(response) 
+        success: function (response)
         {
             result = response;
         },
-        error: function(req, status, error) {
+        error: function (req, status, error) {
             console.err(error);
         }
     });
@@ -181,9 +216,9 @@ function canSwapTwoEvents(event_source, event_destination)
 function resetEventsColor(calEvent)
 {
     var array_events_calendar = $('#calendar-holder').fullCalendar('clientEvents');
-    for(var i =0; i<array_events_calendar.length; i++)
+    for (var i = 0; i < array_events_calendar.length; i++)
     {
-        if(array_events_calendar[i].id != calEvent.id)
+        if (array_events_calendar[i].id != calEvent.id)
         {
             array_events_calendar[i].color = "";
         }
@@ -193,12 +228,55 @@ function resetEventsColor(calEvent)
 function markUpAlterableEvents(calEvent)
 {
     var array_events_calendar = $('#calendar-holder').fullCalendar('clientEvents');
-    for(var i =0; i<array_events_calendar.length; i++)
+    var array_objects_events = new Array();
+    for (var i = 0; i < array_events_calendar.length; i++)
     {
-        if(array_events_calendar[i].id != calEvent.id)
+        if (array_events_calendar[i].id != calEvent.id)
         {
-            if(canSwapTwoEvents(two_selected_events[0], array_events_calendar[i]))
-                array_events_calendar[i].color = "green";
+            array_objects_events.push({id: array_events_calendar[i].id, day: array_events_calendar[i].day, week: array_events_calendar[i].week, formation: array_events_calendar[i].formation});
+        }
+
+    }
+    var array_return_values = canSwapTwoEvents(two_selected_events[0], array_objects_events);
+    for (var i = 0; i < array_return_values.length; i++)
+    {
+        if (array_return_values[i].result)
+        {
+            for (var j = 0; j < array_events_calendar.length; j++)
+            {
+                if (array_events_calendar[j].id == array_return_values[i].id)
+                    array_events_calendar[j].color = "green";
+            }
         }
     }
+}
+
+function swapTwoEvents(event_source, event_destination)
+{
+    $.ajax({
+        type: "POST",
+        async: false,
+        url: Routing.generate('swap_two_events'),
+        data: {
+            event_source: {id: event_source.id, day: event_source.day, week: event_source.week, formation: event_source.formation},
+            event_destination: {id: event_destination.id, day: event_destination.day, week: event_destination.week, formation: event_destination.formation}
+        },
+        success: function (response)
+        {
+            if (response)
+            {
+                console.log(two_selected_events);
+                refreshCalendarEvents();
+                two_selected_events = [];
+
+                console.log(two_selected_events);
+            }
+            else {
+                alert("Une erreur s'est produite lors du chagement des créneaux");
+            }
+        },
+        error: function (req, status, error) {
+            console.err(error);
+        }
+    });
 }
