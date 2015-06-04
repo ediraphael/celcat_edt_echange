86262c1df51b6f0ba0b75912dc2c3f8e04e1fc2e
raphael <raphael.pillie@gmail.com>
2015-06-04 10:20:08 +0200

    Ajout d'un formulaire pour usermail

---
+++ CelcatManager/src/CelcatManagement/AppBundle/Form/UserMailType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace CelcatManagement\AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserMailType extends EmailType
+{
+    
+}
