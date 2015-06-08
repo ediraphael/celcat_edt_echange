03c6d75195c4ab5ce8c6117b331859fda9aa3d9b
raphael <raphael.pillie@gmail.com>
2015-06-08 14:24:23 +0200

    Correction du calendar Controller

--- CelcatManager/src/CelcatManagement/AppBundle/Controller/CalendarController.php
+++ CelcatManager/src/CelcatManagement/AppBundle/Controller/CalendarController.php
@@ -105,7 +105,7 @@ class CalendarController extends Controller {
         
         $return_events = array();
         foreach ($scheduleModifications as $cle => $scheduleModification) {
-            $return_events[$cle] = $scheduleModification->toArray();
+            $return_events[] = $scheduleModification->toArray();
         }
         
         $response = new \Symfony\Component\HttpFoundation\Response();
