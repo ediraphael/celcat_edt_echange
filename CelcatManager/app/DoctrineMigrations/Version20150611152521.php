ddc55e98f2f53fe1a3cadd14df79ca160cdf331a
raphael <raphael.pillie@gmail.com>
2015-06-11 15:25:34 +0200

    Ajout d'un fichier de migration

---
+++ CelcatManager/app/DoctrineMigrations/Version20150611152521.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20150611152521 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE event_modification ADD start_datetime_initial DATE NOT NULL, ADD end_datetime_initial DATE NOT NULL, ADD start_datetime_final DATE NOT NULL, ADD end_datetime_final DATE NOT NULL, DROP date_initial, DROP date_final');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE event_modification ADD date_initial DATE NOT NULL, ADD date_final DATE NOT NULL, DROP start_datetime_initial, DROP end_datetime_initial, DROP start_datetime_final, DROP end_datetime_final');
+    }
+}
