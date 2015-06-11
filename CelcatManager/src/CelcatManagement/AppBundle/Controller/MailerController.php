ad1a07313e53c1d7a5bb5fa0a2d3cfc803f1b97d
raphael <raphael.pillie@gmail.com>
2015-06-11 18:03:18 +0200

    Ajout de méthode dans le mailer pour envoyer des mails

--- CelcatManager/src/CelcatManagement/AppBundle/Controller/MailerController.php
+++ CelcatManager/src/CelcatManagement/AppBundle/Controller/MailerController.php
@@ -25,6 +25,80 @@ class MailerController extends Controller {
         ));
     }
 
+    public function sendUnsendedMailAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->getUser();
+        /* @var $user \CelcatManagement\AppBundle\Security\User */
+
+        $entity = $em->getRepository('CelcatManagementAppBundle:UserMail')->findOneBy(array(
+            'user' => $user->getUsername(),
+            'sended' => 0
+        ));
+        /* @var $entity UserMail */
+        
+        if($entity == null) {
+            return $this->redirect($this->generateUrl('celcat_management_app_schedulemodification'));
+        }
+
+        $form = $this->createForm(new UserMailType($user->getMail()), $entity, array(
+            'action' => $this->generateUrl('celcat_management_app_mailer_sendmodification'),
+            'method' => 'POST',
+        ));
+        $form->add('submit', 'submit', array('label' => 'Envoyer'));
+
+        return $this->render('CelcatManagementAppBundle:Mailer:index.html.twig', array(
+                    'form' => $form->createView()
+        ));
+    }
+
+    public function sendModificationAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->getUser();
+        /* @var $user \CelcatManagement\AppBundle\Security\User */
+        $email = $em->getRepository('CelcatManagementAppBundle:UserMail')->findOneBy(array(
+            'user' => $user->getUsername(),
+            'sended' => 0
+        ));
+
+        $form = $this->createForm(new UserMailType($user->getMail()), $email, array(
+            'action' => $this->generateUrl('celcat_management_app_mailer_sendmodification'),
+            'method' => 'POST',
+        ));
+        $form->add('submit', 'submit', array('label' => 'Envoyer'));
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $message = \Swift_Message::newInstance()
+                    ->setSubject($email->getSubject())
+                    ->setFrom($email->getFromAddress())
+                    ->setTo($email->getToAddress())
+                    ->setBody($email->getBody())
+            ;
+            $failed = array();
+            //$result = $this->get('mailer')->send($message, $failed);
+            $email->setSended(true);
+
+            $entities = $em->getRepository('CelcatManagementAppBundle:ScheduleModification')->findBy(array(
+                'user' => $user->getUsername(),
+                'mailed' => 0,
+                'validated' => 1,
+                'canceled' => 0
+            ));
+            /* @var $entities \CelcatManagement\AppBundle\Entity\ScheduleModification[] */
+            foreach ($entities as $entity) {
+                $entity->setMailed(true);
+            }
+
+            $em->persist($email);
+            $em->flush();
+            return $this->redirect($this->generateUrl('celcat_management_app_mailer_send_unsend'));
+        }
+
+        return $this->render('CelcatManagementAppBundle:Mailer:index.html.twig', array(
+                    'form' => $form->createView()
+        ));
+    }
+
     public function sendAction(Request $request) {
         $em = $this->getDoctrine()->getManager();
         $email = new UserMail();
@@ -47,6 +121,7 @@ class MailerController extends Controller {
             ;
             $failed = array();
             $result = $this->get('mailer')->send($message, $failed);
+            $email->setSended(true);
             $em->persist($email);
             $em->flush();
         }
