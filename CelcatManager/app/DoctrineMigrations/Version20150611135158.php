3ede5001b599f12af2ce69192b19bde18230d9e9
raphael <raphael.pillie@gmail.com>
2015-06-11 13:56:43 +0200

    Ajout d'un fichier de migration

---
+++ CelcatManager/app/DoctrineMigrations/Version20150611135158.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20150611135158 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE event_modification (id INT AUTO_INCREMENT NOT NULL, event_id VARCHAR(20) NOT NULL, event_titre VARCHAR(50) NOT NULL, professors LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', groupes LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', date_initial DATE NOT NULL, date_final DATE NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE event_modification');
+    }
+}
