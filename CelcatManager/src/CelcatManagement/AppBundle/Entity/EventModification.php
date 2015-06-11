a0fd37bd1e4ccda3a0c2849dbd22d7bf95ea1601
raphael <raphael.pillie@gmail.com>
2015-06-11 15:27:15 +0200

    Mise en place d'une methode de feed pour les event modification

--- CelcatManager/src/CelcatManagement/AppBundle/Entity/EventModification.php
+++ CelcatManager/src/CelcatManagement/AppBundle/Entity/EventModification.php
@@ -66,7 +66,11 @@ class EventModification
         $this->eventTitre = $element->getTitle();
         $this->professors = $element->getProfessors()->toArray();
         $this->groupes = $element->getGroup();
-        $this->dateFinal = $element->getStartDatetime();
+        $this->startDateTimeFinal = $element->getStartDatetime();
+        $this->endDateTimeFinal = $element->getEndDatetime();
+        $element->deleteReplacementEvent();
+        $this->startDateTimeInitial = $element->getStartDatetime();
+        $this->endDateTimeInitial = $element->getEndDatetime();
     }
 
     
