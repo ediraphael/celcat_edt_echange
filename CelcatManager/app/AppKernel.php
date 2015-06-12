43d1333ae22ca1cedebd188a471c7a7467dfe1bb
raphael <raphael.pillie@gmail.com>
2015-06-12 13:20:02 +0200

    Ajout d'un nouveau bundle pour la pagination

--- CelcatManager/app/AppKernel.php
+++ CelcatManager/app/AppKernel.php
@@ -26,6 +26,7 @@ class AppKernel extends Kernel
             new ADesigns\CalendarBundle\ADesignsCalendarBundle(),
             new Genemu\Bundle\FormBundle\GenemuFormBundle(),
             new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
